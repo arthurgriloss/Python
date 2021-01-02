ee6bf91ed9a6468c86378b0e06c8669052f8ed53
arthurgriloss <76848350+arthurgriloss@users.noreply.github.com>
2021-01-02 08:13:33 +0100

    Create Importing Datasets.py
    Exercise done at module 1 to import, format, and save data.

---
+++ Courses/Analizing Data with Python/Importing Datasets.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data'
+df = pd.read_csv(url, header=None)
+
+print(df.head())
+print(df.tail(10))
+
+headers = ["symboling", "normalized-losses", "make", "fuel-type", "aspiration", "num-of-doors", "body-style",
+         "drive-wheels", "engine-location", "wheel-base", "length", "width", "height", "curb-weight", "engine-type",
+         "num-of-cylinders", "engine-size", "fuel-system", "bore", "stroke", "compression-ratio", "horsepower",
+         "peak-rpm", "city-mpg", "highway-mpg", "price"]
+df.columns = headers
+print(df.columns)
+print(df.head(10))
+
+df_edit = df.replace("?", np.NaN)
+df_non_NaN = df.dropna(subset=['price'], axis=0)
+
+print(df_non_NaN.dtypes)
+print(df_non_NaN.describe())
+print(df_non_NaN.describe(include='all'))
+print(df_non_NaN.info())
+
+df.to_csv('automobile.csv', index=False)
